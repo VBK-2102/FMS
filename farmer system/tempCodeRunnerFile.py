50b0467bd8ee24997646717c14b0bb7fb330994c
VAIBHAV KALUNGADA <121569845+VBK-2102@users.noreply.github.com>
2024-06-11 14:28:57 +0530

    Add files via upload

---
+++ farmer system/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+
+app.secret_key='harshithbhaskar'
+
+
+# this is for getting unique user access
+login_manager=LoginManager(app)
+login_manager.login_view='login'
+
+@login_manager.user_loader
+def load_user(user_id):
